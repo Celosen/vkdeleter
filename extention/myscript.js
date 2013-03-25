624e5ae38e8cfd9655980baf0558fce9dba4148c
Celeosen <atf16dtp@gmail.com>
2013-03-26 00:02:24 +0400

    Button added

--- extention/myscript.js
+++ extention/myscript.js
@@ -1,1 +1,13 @@
-alert("hello") 
+var buttonHtml = '<span class="divider">|</span>'+
+	' <span><a id="vkdl" href="#">Удалить все сообщения</a></span>';
+	
+function insertButton () { 
+	 var btn = $('#vkdl');
+	 if (btn.length == 0){
+	 
+		var cont = $('#im_dialogs_summary .summary');
+		cont.append(buttonHtml);
+	}	
+}
+setInterval(insertButton, 200);
+
