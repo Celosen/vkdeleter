646612fbf23623247f4d0ca0c0236060009739d4
Celeosen <atf16dtp@gmail.com>
2013-03-27 00:47:27 +0400

    it works!

--- extention/myscript.js
+++ extention/myscript.js
@@ -1,24 +1,30 @@
 var buttonHtml = '<span class="divider">|</span>'+
-	' <span><a id="vkdl" href="#">Удалить все сообщения</a></span>';
-	
+    ' <span><a id="vkdl" href="#">Удалить все сообщения</a></span>';
+    
 function insertButton () { 
-	 var btn = $('#vkdl');
-	 if (btn.length == 0){
-	 
-		var cont = $('#im_dialogs_summary .summary');
-		cont.append(buttonHtml);
-		
-		btn = $('#vkdl');
-		btn.click (onClick);	
-	}
-		
+     var btn = $('#vkdl');
+     if (btn.length == 0){
+     
+        var cont = $('#im_dialogs_summary .summary');
+        cont.append(buttonHtml);
+        
+        btn = $('#vkdl');
+        btn.click (onClick);    
+    }
+        
 }
-setInterval(insertButton, 200);
+
 
 
 function onClick (){
-	var btnAction = $('.dialogs_del:first').click();
-    setTimeout(btnAction, 30); 
-	var confirm = $('.popup_box_container .button_blue button').click();
-	}
-	
+    var btnAction = $('.dialogs_del:first').click();
+    setTimeout(confirmation, 70); 
+
+}
+
+    
+function confirmation (){   
+    var confirm = $('.popup_box_container .button_blue button').click();
+}
+
+setInterval(insertButton, 200);
